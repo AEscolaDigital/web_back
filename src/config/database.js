b857c35752d4aadd57f8957a388e03047e02ef91
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-09-26 17:05:20 -0300

    Primeiro commit

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+    url: "mysql://root:abc45101922072@localhost:3306/school",
+    config: {
+        dialect: "mysql",
+        define: {
+            timestamp: true,
+            underscored: true
+        }
+    }
+}
