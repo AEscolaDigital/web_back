6c9c7e55d1105f073545658a010ce56537d4442a
AntonioAiltonGocalves <antonioailton723@gmail.com>
2021-10-03 17:52:55 -0300

    the initial commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: "mysql://root:abc45101922072@localhost:3306/school",
+    url: "mysql://root:manolo@localhost:3306/school",
     config: {
         dialect: "mysql",
         define: {
