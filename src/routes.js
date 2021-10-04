d5b9bb3f800b01bbe3ced68b1080603fbbd2bde5
thedigitalschool <91063161+thedigitalschool@users.noreply.github.com>
2021-10-04 11:03:22 -0300

    proteger rotas importantes

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,17 @@
 const routes = require("express").Router();
+const authMiddleware = require("./middleware/autorization")
 
 const StudentController = require("./controllers/student");
 const loginController = require("./controllers/login");
 
+//Rota de login
+routes.post('/login', loginController.store);
+
+routes.use(authMiddleware);
 
 // Rota do aluno
 routes.post('/student', StudentController.store);
 routes.get('/student/:student_id', StudentController.index);
 
-//Rota de login
-routes.post('/login', loginController.store);
 
 module.exports = routes;
