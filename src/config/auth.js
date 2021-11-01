bfc423b74209888ca263476ee6cc980e9a83b546
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-11-01 15:18:21 -0300

    commit

--- src/config/auth.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    //the string is a default password for we email
-    secret: "8acf751224f3324410146a33c17a44dd"
-}
--- src/index.js
+++
@@ -1,5 +0,0 @@
-const app = require("./app");
-
-app.listen(3333, () => {
-    console.log(`Servidor rodando na porta ${3333}`);
-});
