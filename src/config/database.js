851c90afd995d64d0a53429488e7d65828fa257c
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-11-23 20:13:01 -0300

    Tentando corrigir o erro o pg_hba.conf entry for host, SSL off

--- src/config/database.js
+++ src/config/database.js
@@ -4,11 +4,18 @@ module.exports = {
     url: process.env.DATABASE_URL || "mysql://root:bcd127@localhost:3306/school",
     config: {
         dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+         },
         logging: console.log,
         define: {
             timestamp: true,
             underscored: true
         },
+    
     },
    
 }
