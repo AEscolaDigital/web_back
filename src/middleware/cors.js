fd475c20370f6d79ef903cb6c0c21ca04d95c0f3
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-11-24 19:06:40 -0300

    cors

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -2,6 +2,7 @@ const cors = (req, res, next) => {
     res.set('access-control-allow-origin', '*')
     res.set('access-control-allow-methods', '*')
     res.set('access-control-allow-headers', '*')
+
     next()
 }
   
