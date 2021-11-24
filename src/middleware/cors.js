b96b00c0c632025860f545ac5382e185d469c96b
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-11-24 11:24:42 -0300

    Configurando o access-control-allow-origin

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,5 +1,5 @@
 const cors = (req, res, next) => {
-    res.set('access-control-allow-origin', 'https://condescending-lewin-aa5404.netlify.app/')
+    res.set('access-control-allow-origin', '*')
     res.set('access-control-allow-methods', '*')
     res.set('access-control-allow-headers', '*')
     next()
