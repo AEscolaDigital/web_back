fe557db165395ffbec3627d41e0b0d66c70cc224
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-11-24 15:02:01 -0300

    Mudança nos cors

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,8 +1,8 @@
 const cors = (req, res, next) => {
     res.set('access-control-allow-origin', '*')
-    res.set('access-control-allow-methods', '*')
+    res.set('access-control-allow-methods', 'GET,PUT,POST,DELETE')
     res.set('access-control-allow-headers', '*')
     next()
-  }
+}
   
 module.exports = cors
