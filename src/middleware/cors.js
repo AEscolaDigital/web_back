7269c5f012f6ec6c50cc56359a6600b739abaf3e
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-11-24 16:01:14 -0300

    Setando os headers:

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,8 +1,15 @@
 const cors = (req, res, next) => {
-    res.set('access-control-allow-origin', '*')
-    res.set('access-control-allow-methods', 'GET,PUT,POST,DELETE')
-    res.set('access-control-allow-headers', '*')
-    next()
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+    if (req.method === "OPTIONS") {
+      return res.status(200).end();
+    } else {
+      next();
+    }
+  });
 }
-  
+
 module.exports = cors
