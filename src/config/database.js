fea85faea5aa8c9008c0e39d7a0226b65db68273
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-12-18 20:04:47 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,12 @@ module.exports = {
     url: process.env.HEROKU_POSTGRESQL_GREEN_URL,
     config: {
         dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+         },
         define: {
             timestamp: true,
             underscored: true
