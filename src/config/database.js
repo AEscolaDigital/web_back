0c99dbc3f4e501b2361289e9869fd41191a86b7b
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-12-18 19:57:07 -0300

    Migrando o banco de dados para PostgreSQL

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 
 module.exports = {
-    url: process.env.DATABASE_URL,
+    url: process.env.HEROKU_POSTGRESQL_GREEN_URL,
     config: {
-        dialect: "mysql",
+        dialect: "postgres",
         dialectOptions: {
             ssl: {
                 require: true,
